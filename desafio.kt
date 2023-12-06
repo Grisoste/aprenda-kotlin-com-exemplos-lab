f2d1ead1ec43731a29bbda46491c52ac88fefd2a
Gabriel Grisoste de Souza <139875438+Grisoste@users.noreply.github.com>
2023-12-06 10:17:53 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,54 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+        println("Conteúdo '${conteudo.nome}' adicionado à formação $nome.")
+    }
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na formação $nome.")
+    }
+
+    fun exibirConteudos() {
+        println("Conteúdos da formação $nome:")
+        for (conteudo in conteudos) {
+            println("${conteudo.nome} - Duração: ${conteudo.duracao} minutos")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+
+    
+    val kotlinBasico = ConteudoEducacional("Introdução ao Kotlin", 60)
+    val kotlinIntermediario = ConteudoEducacional("Programação Kotlin Intermediária", 90)
+    val kotlinAvancado = ConteudoEducacional("Desenvolvimento Avançado em Kotlin", 120)
+
+    
+    val formacaoKotlin = Formacao("Formação Kotlin")
+
+    
+    formacaoKotlin.adicionarConteudo(kotlinBasico)
+    formacaoKotlin.adicionarConteudo(kotlinIntermediario)
+    formacaoKotlin.adicionarConteudo(kotlinAvancado)
+
+    
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+    
+    formacaoKotlin.exibirConteudos()
+    println("Usuários matriculados na formação ${formacaoKotlin.nome}: ${formacaoKotlin.inscritos.map { it.nome }}")
 }
